31fbc7aad596d7beb29faf990ce481f1965b3e58
hutch445 <65507983+hutch445@users.noreply.github.com>
2021-03-14 21:01:51 -0400

    Added code for zoom preferences and mental health

--- src/App.java
+++ src/App.java
@@ -27,10 +27,50 @@ public class App {
 	}
 	
 	public static void mentalHealth() {
-		
+		Scanner myScanner = new Scanner(System.in);
+		System.out.println("Please answer the following question on a scale from 1-10.");
+		System.out.println("How ready do you feel for today?");
+		int ready = myScanner.nextInt();
+		System.out.println("Are you looking forward to today?");
+		int forward = myScanner.nextInt();
+		System.out.println("How are you feeling mentally?");
+		int mental = myScanner.nextInt();
+		System.out.println("How are you feeling physically?");
+		int physical = myScanner.nextInt();
+		System.out.println("How much energy do you have to give today?");
+		int energy = myScanner.nextInt();
+		System.out.println("How happy are you today?");
+		int happiness = myScanner.nextInt();
+		int total = ready+forward+mental+physical+energy+happiness;
+		if (total < 30)
+		{
+			System.out.println("You should talk to someone about how you are feeling");
+		} 
+		else if (total < 40)
+		{
+			System.out.println("I hope your day goes well!");
+		}
+		else
+		{
+			System.out.println("I'm glad you are having a good start to your day! Keep it up!");
+		}
+		myScanner.close();
 	}
 	
 	public static void zoomPreferences() {
-		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Please answer the following questions.");
+		System.out.println("On a scale from 1-10, how much will you participate in class today?");
+		int participate = scan.nextInt();
+		System.out.println("Will you put your camera on in class? 1 for yes, 2 for no.");
+		int camera = scan.nextInt();
+		if (camera == 1)
+		{
+			System.out.println("Great! Can't wait to see you in class today!");
+		}
+		else
+		{
+			System.out.println("Alright maybe another time, I hope your classes go well today!");
+		}
 	}
 }
